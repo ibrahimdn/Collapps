66a586007f3c2a3e1b32ddc05002f5cafcc743e6
ibrahim doğan <ibrahim.dogan@digitus.com.tr>
2019-06-17 13:22:55 +0300

    Initial Commit

---
+++ label collapse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  label collapse
+//
+//  Created by ibrahim doğan on 17.06.2019.
+//  Copyright © 2019 Tabbar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
